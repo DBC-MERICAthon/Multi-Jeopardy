6c32d74776407d4ea38e26ce4fefb1ee1ef3d851
Jordan Leigh <AlwaysBCoding@gmail.com>
2015-07-01 09:41:15 -0700

    configures react

--- jeopardybackend/config/environments/development.rb
+++ jeopardybackend/config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  Jeopardybackend::Application.configure do
+    config.react.variant = :development
+  end
+
 end
