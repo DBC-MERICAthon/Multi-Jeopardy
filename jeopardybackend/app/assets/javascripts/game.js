cf95a68aa746b2e8db82f4655f5a084a5a24689a
Tim Cannady <timcannady@gmail.com>
2015-07-01 13:48:04 -0700

    interactive layout

--- jeopardybackend/app/assets/javascripts/game.js
+++ jeopardybackend/app/assets/javascripts/game.js
@@ -1,48 +1,5 @@
 var firebaseClient = new Firebase("https://dbc-jeopardy.firebaseio.com/");
-var gameStateRef = new Firebase("https://dbc-jeopardy.firebaseio.com/game_state");
-
-// var mockGameStateData = {
-//   board: {
-//     clue_a_1: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_b_1: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_c_1: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_d_1: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_e_1: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_f_1: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-
-//     clue_a_2: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_b_2: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_c_2: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_d_2: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_e_2: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_f_2: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-
-//     clue_a_3: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_b_3: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_c_3: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_d_3: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_e_3: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_f_3: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-
-//     clue_a_4: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_b_4: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_c_4: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_d_4: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_e_4: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_f_4: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-
-//     clue_a_5: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_b_5: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_c_5: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_d_5: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_e_5: {status: "ready", questionText: "", answerText: "", pointValue: 0},
-//     clue_f_5: {status: "ready", questionText: "", answerText: "", pointValue: 0}
-//   },
-
-//   phase: "choose-question",
-
-//   connected_players: []
-// }
+var gameStateRef = new Firebase("https://dbc-jeopardy.firebaseio.com/games/12" );
 
 // choose-question
 // question-timer
@@ -53,27 +10,95 @@ $(function() {
 
 if($("body").hasClass("games-show")) {
 
+  // Clue
+  var clue = React.createClass({
+    render: function() {
+      switch(this.props.clue.status) {
+        case "ready":
+          return React.createElement("td", {className: "clue-value"}, "$" + this.props.clue.pointValue)
+        case "current":
+          return React.createElement("td", {}, this.props.clue.questionText)
+        case "answered":
+          return React.createElement("td", {}, "")
+      }
+    }
+  })
+
   // Board
   var board = React.createClass({
-    render: function( ) {
-      return React.DOM.h1({}, "This is the board");
+    render: function() {
+      if(!_.isEmpty(this.props.board)) {
+        return React.createElement("table", {},
+          React.createElement("thead", {},
+            React.createElement("tr", {},
+              React.createElement("th", {}, this.props.board.category_a),
+              React.createElement("th", {}, this.props.board.category_b),
+              React.createElement("th", {}, this.props.board.category_c),
+              React.createElement("th", {}, this.props.board.category_d),
+              React.createElement("th", {}, this.props.board.category_e),
+              React.createElement("th", {}, this.props.board.category_f)
+            )
+          ),
+          React.createElement("tbody", {},
+            React.createElement("tr", {},
+              React.createElement(clue, {clue: this.props.board.clue_a_1}),
+              React.createElement(clue, {clue: this.props.board.clue_b_1}),
+              React.createElement(clue, {clue: this.props.board.clue_c_1}),
+              React.createElement(clue, {clue: this.props.board.clue_d_1}),
+              React.createElement(clue, {clue: this.props.board.clue_e_1}),
+              React.createElement(clue, {clue: this.props.board.clue_f_1})
+            ),
+            React.createElement("tr", {},
+              React.createElement(clue, {clue: this.props.board.clue_a_2}),
+              React.createElement(clue, {clue: this.props.board.clue_b_2}),
+              React.createElement(clue, {clue: this.props.board.clue_c_2}),
+              React.createElement(clue, {clue: this.props.board.clue_d_2}),
+              React.createElement(clue, {clue: this.props.board.clue_e_2}),
+              React.createElement(clue, {clue: this.props.board.clue_f_2})
+            ),
+            React.createElement("tr", {},
+              React.createElement(clue, {clue: this.props.board.clue_a_3}),
+              React.createElement(clue, {clue: this.props.board.clue_b_3}),
+              React.createElement(clue, {clue: this.props.board.clue_c_3}),
+              React.createElement(clue, {clue: this.props.board.clue_d_3}),
+              React.createElement(clue, {clue: this.props.board.clue_e_3}),
+              React.createElement(clue, {clue: this.props.board.clue_f_3})
+            ),
+            React.createElement("tr", {},
+              React.createElement(clue, {clue: this.props.board.clue_a_4}),
+              React.createElement(clue, {clue: this.props.board.clue_b_4}),
+              React.createElement(clue, {clue: this.props.board.clue_c_4}),
+              React.createElement(clue, {clue: this.props.board.clue_d_4}),
+              React.createElement(clue, {clue: this.props.board.clue_e_4}),
+              React.createElement(clue, {clue: this.props.board.clue_f_4})
+            ),
+            React.createElement("tr", {},
+              React.createElement(clue, {clue: this.props.board.clue_a_5}),
+              React.createElement(clue, {clue: this.props.board.clue_b_5}),
+              React.createElement(clue, {clue: this.props.board.clue_c_5}),
+              React.createElement(clue, {clue: this.props.board.clue_d_5}),
+              React.createElement(clue, {clue: this.props.board.clue_e_5}),
+              React.createElement(clue, {clue: this.props.board.clue_f_5})
+            )
+          )
+        );
+      } else {
+        return React.createElement("p", {}, "LOADING...")
+      }
     }
   });
 
   // Connected Users Section
 
   // Popover Question View (clue where, state is active)
-  var clue = React.createClass({
-    render: function() {
-      return React.DOM.p({}, this.props.clue.questionText)
-    }
-  })
 
   // Game
   var game = React.createClass({
     getInitialState: function() {
       return {
-        gameState: {}
+        gameState: {
+          board: {}
+        }
       }
     },
 
@@ -85,19 +110,15 @@ if($("body").hasClass("games-show")) {
     },
 
     render: function() {
-      var currentClues = _.filter(this.state.gameState.board, function(value, key) {
-        return value.status == "current"
-      });
+      return React.DOM.div({},
+          React.createElement(board, {board: this.state.gameState.board})
+      )
 
-      var currentClue = _.isEmpty(currentClues) ? null : _.first(currentClues);
+      // var currentClues = _.filter(this.state.gameState.board, function(value, key) {
+      //   return value.status == "current"
+      // });
 
-      if (currentClue) {
-        return React.createElement(clue, {clue: currentClue})
-      } else {
-        return React.DOM.div({},
-          React.createElement(board, {gameState: this.state.gameState})
-        )
-      }
+      // var currentClue = _.isEmpty(currentClues) ? null : _.first(currentClues);
     },
 
     setGameState: function(gameState) {
@@ -118,130 +139,3 @@ if($("body").hasClass("games-show")) {
 
 
 } })
-
-// <div class = "connected-players">
-// </div>
-
-//   <table >
-
-//   <thead>
-//     <tr>
-//       <% @single_jeopardy_clue_groups.each do |clue_group| %>
-//         <th><%= "#{clue_group.category.name} (#{clue_group.year})"%></th>
-//       <% end %>
-//     </tr>
-//   </thead>
-
-//   <tbody>
-//     <tr>
-//         <% @single_jeopardy_clue_groups.each do |clue_group| %>
-//           <!-- <td><%= clue_group.clues.where(point_value: 200).first.question %></td> -->
-//           <td class="clue-value">$200</td>
-//         <% end %>
-//     </tr>
-//     <tr>
-//         <% @single_jeopardy_clue_groups.each do |clue_group| %>
-//           <!-- <td><%= clue_group.clues.where(point_value: 400).first.question %></td> -->
-//           <td class="clue-value">$400</td>
-//         <% end %>
-//     </tr>
-//     <tr>
-//         <% @single_jeopardy_clue_groups.each do |clue_group| %>
-//           <!-- <td><%= clue_group.clues.where(point_value: 600).first.question %></td> -->
-//           <td class="clue-value">$600</td>
-//         <% end %>
-//     </tr>
-//     <tr>
-//         <% @single_jeopardy_clue_groups.each do |clue_group| %>
-//           <!-- <td><%= clue_group.clues.where(point_value: 800).first.question %></td> -->
-//           <td class="clue-value">$800</td>
-//         <% end %>
-//     </tr>
-//     <tr>
-//         <% @single_jeopardy_clue_groups.each do |clue_group| %>
-//           <!-- <td><%= clue_group.clues.where(point_value: 1000).first.question %></td> -->
-//           <td class="clue-value">$1000</td>
-//         <% end %>
-//     </tr>
-//   </tbody>
-// </table>
-
-// <br />
-// <br />
-// <br />
-// <br />
-// <br />
-
-//   <table >
-
-//   <thead>
-//     <tr>
-//       <% @double_jeopardy_clue_groups.each do |clue_group| %>
-//         <th><%= "#{clue_group.category.name} (#{clue_group.year})"%></th>
-//       <% end %>
-//     </tr>
-//   </thead>
-
-//   <tbody>
-//     <tr>
-//         <% @double_jeopardy_clue_groups.each do |clue_group| %>
-//           <!-- <td><%= clue_group.clues.where(point_value: 400).first.question %></td> -->
-//           <td class="clue-value">$400</td>
-//         <% end %>
-//     </tr>
-//     <tr>
-//         <% @double_jeopardy_clue_groups.each do |clue_group| %>
-//           <!-- <td><%= clue_group.clues.where(point_value: 800).first.question %></td> -->
-//           <td class="clue-value">$800</td>
-//         <% end %>
-//     </tr>
-//     <tr>
-//         <% @double_jeopardy_clue_groups.each do |clue_group| %>
-//           <!-- <td><%= clue_group.clues.where(point_value: 1200).first.question %></td> -->
-//           <td class="clue-value">$1200</td>
-//         <% end %>
-//     </tr>
-//     <tr>
-//         <% @double_jeopardy_clue_groups.each do |clue_group| %>
-//           <!-- <td><%= clue_group.clues.where(point_value: 1600).first.question %></td> -->
-//           <td class="clue-value">$1600</td>
-//         <% end %>
-//     </tr>
-//     <tr>
-//         <% @double_jeopardy_clue_groups.each do |clue_group| %>
-//           <!-- <td><%= clue_group.clues.where(point_value: 2000).first.question %></td> -->
-//           <td class="clue-value">$2000</td>
-//         <% end %>
-//     </tr>
-//   </tbody>
-// </table>
-
-
-// <!-- Avatar stuff from SemanticUI -->
-// <div class="ui mini horizontal divided list">
-//   <div class="item">
-//     <div class="ui avatar image">
-//       <%= image_tag "daniel.jpg" %>
-//     </div>
-//     <div class="content">
-//       <a class="header">Helen</a>
-//     </div>
-//   </div>
-//   <div class="item">
-//     <div class="ui avatar image">
-//       <%= image_tag "helen.jpg" %>
-//     </div>
-//     <div class="content">
-//       <a class="header">Christian</a>
-//     </div>
-//   </div>
-//   <div class="item">
-//     <div class="ui avatar image">
-//       <%= image_tag "christian.jpg" %>
-//     </div>
-//     <div class="content">
-//       <a class="header">Daniel</a>
-//     </div>
-//   </div>
-// </div>
-// <!-- Avatar stuff from SemanticUI -->
--- jeopardybackend/app/models/game.rb
+++ jeopardybackend/app/models/game.rb
@@ -1,3 +1,61 @@
 class Game < ActiveRecord::Base
 
+  after_create :sync_to_firebase
+
+  def generate_game_state
+    single_jeopardy_clue_groups = self.single_jeopardy_clue_groups.split(",").map { |id| ClueGroup.find(id) }
+    double_jeopardy_clue_groups = self.single_jeopardy_clue_groups.split(",").map { |id| ClueGroup.find(id) }
+
+    {phase: "choose-question",
+     board: {
+      category_a: single_jeopardy_clue_groups[0].category.name,
+      category_b: single_jeopardy_clue_groups[1].category.name,
+      category_c: single_jeopardy_clue_groups[2].category.name,
+      category_d: single_jeopardy_clue_groups[3].category.name,
+      category_e: single_jeopardy_clue_groups[4].category.name,
+      category_f: single_jeopardy_clue_groups[5].category.name,
+
+      clue_a_1: {pointValue: 200, questionText: single_jeopardy_clue_groups[0].clues.where(point_value: 200).first.question, answerText: single_jeopardy_clue_groups[0].clues.where(point_value: 200).first.answer, status: "ready"},
+      clue_a_2: {pointValue: 400, questionText: single_jeopardy_clue_groups[0].clues.where(point_value: 400).first.question, answerText: single_jeopardy_clue_groups[0].clues.where(point_value: 400).first.answer, status: "ready"},
+      clue_a_3: {pointValue: 600, questionText: single_jeopardy_clue_groups[0].clues.where(point_value: 600).first.question, answerText: single_jeopardy_clue_groups[0].clues.where(point_value: 600).first.answer, status: "ready"},
+      clue_a_4: {pointValue: 800, questionText: single_jeopardy_clue_groups[0].clues.where(point_value: 800).first.question, answerText: single_jeopardy_clue_groups[0].clues.where(point_value: 800).first.answer, status: "ready"},
+      clue_a_5: {pointValue: 1000, questionText: single_jeopardy_clue_groups[0].clues.where(point_value: 1000).first.question, answerText: single_jeopardy_clue_groups[0].clues.where(point_value: 1000).first.answer, status: "ready"},
+
+      clue_b_1: {pointValue: 200, questionText: single_jeopardy_clue_groups[1].clues.where(point_value: 200).first.question, answerText: single_jeopardy_clue_groups[1].clues.where(point_value: 200).first.answer, status: "ready"},
+      clue_b_2: {pointValue: 400, questionText: single_jeopardy_clue_groups[1].clues.where(point_value: 400).first.question, answerText: single_jeopardy_clue_groups[1].clues.where(point_value: 400).first.answer, status: "ready"},
+      clue_b_3: {pointValue: 600, questionText: single_jeopardy_clue_groups[1].clues.where(point_value: 600).first.question, answerText: single_jeopardy_clue_groups[1].clues.where(point_value: 600).first.answer, status: "ready"},
+      clue_b_4: {pointValue: 800, questionText: single_jeopardy_clue_groups[1].clues.where(point_value: 800).first.question, answerText: single_jeopardy_clue_groups[1].clues.where(point_value: 800).first.answer, status: "ready"},
+      clue_b_5: {pointValue: 1000, questionText: single_jeopardy_clue_groups[1].clues.where(point_value: 1000).first.question, answerText: single_jeopardy_clue_groups[1].clues.where(point_value: 1000).first.answer, status: "ready"},
+
+      clue_c_1: {pointValue: 200, questionText: single_jeopardy_clue_groups[2].clues.where(point_value: 200).first.question, answerText: single_jeopardy_clue_groups[2].clues.where(point_value: 200).first.answer, status: "ready"},
+      clue_c_2: {pointValue: 400, questionText: single_jeopardy_clue_groups[2].clues.where(point_value: 400).first.question, answerText: single_jeopardy_clue_groups[2].clues.where(point_value: 400).first.answer, status: "ready"},
+      clue_c_3: {pointValue: 600, questionText: single_jeopardy_clue_groups[2].clues.where(point_value: 600).first.question, answerText: single_jeopardy_clue_groups[2].clues.where(point_value: 600).first.answer, status: "ready"},
+      clue_c_4: {pointValue: 800, questionText: single_jeopardy_clue_groups[2].clues.where(point_value: 800).first.question, answerText: single_jeopardy_clue_groups[2].clues.where(point_value: 800).first.answer, status: "ready"},
+      clue_c_5: {pointValue: 1000, questionText: single_jeopardy_clue_groups[2].clues.where(point_value: 1000).first.question, answerText: single_jeopardy_clue_groups[2].clues.where(point_value: 1000).first.answer, status: "ready"},
+
+      clue_d_1: {pointValue: 200, questionText: single_jeopardy_clue_groups[3].clues.where(point_value: 200).first.question, answerText: single_jeopardy_clue_groups[3].clues.where(point_value: 200).first.answer, status: "ready"},
+      clue_d_2: {pointValue: 400, questionText: single_jeopardy_clue_groups[3].clues.where(point_value: 400).first.question, answerText: single_jeopardy_clue_groups[3].clues.where(point_value: 400).first.answer, status: "ready"},
+      clue_d_3: {pointValue: 600, questionText: single_jeopardy_clue_groups[3].clues.where(point_value: 600).first.question, answerText: single_jeopardy_clue_groups[3].clues.where(point_value: 600).first.answer, status: "ready"},
+      clue_d_4: {pointValue: 800, questionText: single_jeopardy_clue_groups[3].clues.where(point_value: 800).first.question, answerText: single_jeopardy_clue_groups[3].clues.where(point_value: 800).first.answer, status: "ready"},
+      clue_d_5: {pointValue: 1000, questionText: single_jeopardy_clue_groups[3].clues.where(point_value: 1000).first.question, answerText: single_jeopardy_clue_groups[3].clues.where(point_value: 1000).first.answer, status: "ready"},
+
+      clue_e_1: {pointValue: 200, questionText: single_jeopardy_clue_groups[4].clues.where(point_value: 200).first.question, answerText: single_jeopardy_clue_groups[4].clues.where(point_value: 200).first.answer, status: "ready"},
+      clue_e_2: {pointValue: 400, questionText: single_jeopardy_clue_groups[4].clues.where(point_value: 400).first.question, answerText: single_jeopardy_clue_groups[4].clues.where(point_value: 400).first.answer, status: "ready"},
+      clue_e_3: {pointValue: 600, questionText: single_jeopardy_clue_groups[4].clues.where(point_value: 600).first.question, answerText: single_jeopardy_clue_groups[4].clues.where(point_value: 600).first.answer, status: "ready"},
+      clue_e_4: {pointValue: 800, questionText: single_jeopardy_clue_groups[4].clues.where(point_value: 800).first.question, answerText: single_jeopardy_clue_groups[4].clues.where(point_value: 800).first.answer, status: "ready"},
+      clue_e_5: {pointValue: 1000, questionText: single_jeopardy_clue_groups[4].clues.where(point_value: 1000).first.question, answerText: single_jeopardy_clue_groups[4].clues.where(point_value: 1000).first.answer, status: "ready"},
+
+      clue_f_1: {pointValue: 200, questionText: single_jeopardy_clue_groups[5].clues.where(point_value: 200).first.question, answerText: single_jeopardy_clue_groups[5].clues.where(point_value: 200).first.answer, status: "ready"},
+      clue_f_2: {pointValue: 400, questionText: single_jeopardy_clue_groups[5].clues.where(point_value: 400).first.question, answerText: single_jeopardy_clue_groups[5].clues.where(point_value: 400).first.answer, status: "ready"},
+      clue_f_3: {pointValue: 600, questionText: single_jeopardy_clue_groups[5].clues.where(point_value: 600).first.question, answerText: single_jeopardy_clue_groups[5].clues.where(point_value: 600).first.answer, status: "ready"},
+      clue_f_4: {pointValue: 800, questionText: single_jeopardy_clue_groups[5].clues.where(point_value: 800).first.question, answerText: single_jeopardy_clue_groups[5].clues.where(point_value: 800).first.answer, status: "ready"},
+      clue_f_5: {pointValue: 1000, questionText: single_jeopardy_clue_groups[5].clues.where(point_value: 1000).first.question, answerText: single_jeopardy_clue_groups[5].clues.where(point_value: 1000).first.answer, status: "ready"}
+      }}
+  end
+
+  def sync_to_firebase
+    firebase = Firebase::Client.new("https://dbc-jeopardy.firebaseio.com")
+    firebase.set("/games/#{self.id}", self.generate_game_state)
+  end
+
 end
