70f6b76a01df3e4f4ac8a329cb187383bcef6e44
Tim Cannady <timcannady@gmail.com>
2015-07-01 15:19:20 -0700

    connected players displaying on screen

--- jeopardybackend/app/assets/javascripts/game.js
+++ jeopardybackend/app/assets/javascripts/game.js
@@ -1,14 +1,66 @@
 var firebaseClient = new Firebase("https://dbc-jeopardy.firebaseio.com/");
-var gameStateRef = new Firebase("https://dbc-jeopardy.firebaseio.com/games/12" );
+var gameStateRef = new Firebase("https://dbc-jeopardy.firebaseio.com/games/" + _.last(window.location.pathname.split("/")));
+var connectedPlayersRef = new Firebase("https://dbc-jeopardy.firebaseio.com/games/" + _.last(window.location.pathname.split("/")) + "/connected_players");
 
 // choose-question
 // question-timer
 // waiting-for-buzz
 // waiting-for-confirmation
 
+var avatars = [
+  'http://images.clipartpanda.com/pizza-slice-graphic-slice_of_pepperoni_pizza_0515-0901-2114-1926_SMU.jpg',
+  'http://img2.wikia.nocookie.net/__cb20100210020028/assassinscreed/images/c/cf/AwesomeSmileySmall.png',
+  'http://avatarbox.net/avatars/img29/cat_dancing_avatar_picture_47092.gif',
+  'http://orig00.deviantart.net/2890/f/2011/037/4/5/the_black_cat_avatar_by_laurasan-d38yxj2.gif',
+  'http://www.animateit.net/data/media/august2009/th_dance.gif'
+]
+
+var getRandomInt = function(min, max) {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 $(function() {
 
 if($("body").hasClass("games-show")) {
+
+  // username = ""
+
+  // if(localStorage.getItem("username")) {
+  //   username = localStorage.getItem("username");
+  // } else {
+  //   localStorage.setItem("username", username);
+  // }
+
+  username = prompt("What's your username");
+
+  var userData = {
+    username: username,
+    avatarUrl: avatars[getRandomInt(0,avatars.length)],
+    points: 0
+  }
+
+  var userRef = connectedPlayersRef.push(userData);
+  userRef.onDisconnect().remove();
+
+  // Connected Users
+  var connectedPlayers = React.createClass({
+    render: function() {
+
+      return React.createElement("div", {className: "connected-players"},
+        _.map(this.props.connected_players, function(player) {
+          return React.createElement("div", {className: "ui card"},
+            React.createElement("a", {className: "image", href: "#"},
+              React.createElement("image", {src: player.avatarUrl})
+            ),
+            React.createElement("div", {className: "content"},
+              React.createElement("p", {className: "header player-username"}, player.username),
+              React.createElement("p", {className: "header player-score"}, "$" + player.points)
+            )
+          )
+        })
+      )
+    }
+  });
 
   // Clue
   var clue = React.createClass({
@@ -22,7 +74,7 @@ if($("body").hasClass("games-show")) {
           return React.createElement("td", {}, "")
       }
     }
-  })
+  });
 
   // Board
   var board = React.createClass({
@@ -88,8 +140,6 @@ if($("body").hasClass("games-show")) {
     }
   });
 
-  // Connected Users Section
-
   // Popover Question View (clue where, state is active)
   var showClue = React.createClass({
     render: function() {
@@ -104,7 +154,8 @@ if($("body").hasClass("games-show")) {
     getInitialState: function() {
       return {
         gameState: {
-          board: {}
+          board: {},
+          connected_players: []
         }
       }
     },
@@ -117,6 +168,8 @@ if($("body").hasClass("games-show")) {
     },
 
     render: function() {
+      // console.log(this.state.gameState.connected_players);
+
       var currentClues = _.filter(this.state.gameState.board, function(value, key) {
         return value.status == "current"
       });
@@ -126,8 +179,9 @@ if($("body").hasClass("games-show")) {
       if (currentClue) {
         return React.createElement(showClue, {clue: currentClue})
       } else {
-        return React.DOM.div({},
-            React.createElement(board, {board: this.state.gameState.board})
+        return React.createElement("div", {className: "container"},
+            React.createElement(board, {board: this.state.gameState.board}),
+            React.createElement(connectedPlayers, {connected_players: this.state.gameState.connected_players})
         )
       }
     },
@@ -141,12 +195,5 @@ if($("body").hasClass("games-show")) {
   });
 
   React.render(React.createElement(game), document.body);
-
-
-
-
-
-
-
 
 } })
--- jeopardybackend/app/assets/javascripts/main.js
+++ jeopardybackend/app/assets/javascripts/main.js
@@ -1,32 +1,33 @@
-var firebaseClient = new Firebase("https://dbc-jeopardy.firebaseio.com/");
-var connected_players = new Firebase("https://dbc-jeopardy.firebaseio.com/connected_players");
+// var firebaseClient = new Firebase("https://dbc-jeopardy.firebaseio.com/");
+// var connected_players = new Firebase("https://dbc-jeopardy.firebaseio.com/connected_players");
 
-username = ""
+// username = ""
 
-if(localStorage.getItem("username")) {
-  username = localStorage.getItem("username");
-} else {
-  username = prompt("What's your username");
-  localStorage.setItem("username", username);
-}
+// if(localStorage.getItem("username")) {
+//   username = localStorage.getItem("username");
+// } else {
+//   username = prompt("What's your username");
+//   localStorage.setItem("username", username);
+// }
 
-var userData = {
-  username: username
-}
+// var userData = {
+//   username: username,
+//   avatarUrl: "http://img2.wikia.nocookie.net/__cb20100210020028/assassinscreed/images/c/cf/AwesomeSmileySmall.png"
+// }
 
-var userRef = connected_players.push(userData);
-userRef.onDisconnect().remove();
+// var userRef = connected_players.push(userData);
+// userRef.onDisconnect().remove();
 
-var playerToHtml = function(player) {
-  return ("<div class='item'>" + player.username + "</div>");
-}
+// var playerToHtml = function(player) {
+//   return ("<div class='item'>" + player.username + "</div>");
+// }
 
-connected_players.on("value", function(snapshot) {
-  players = snapshot.val();
-  var playerText = ""
-  _.each(players, function(player) {
-    playerText += playerToHtml(player);
-  })
+// connected_players.on("value", function(snapshot) {
+//   players = snapshot.val();
+//   var playerText = ""
+//   _.each(players, function(player) {
+//     playerText += playerToHtml(player);
+//   })
 
-  $(".connected-players").html(playerText);
-})
+//   $(".connected-players").html(playerText);
+// })
